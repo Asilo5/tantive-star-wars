da6c9aec70b8046c1bc9031801497b47a7781f87
Allison Wagner <allisonjw01@gmail.com>
2019-12-03 13:53:51 -0700

    Added movie card into movie container

---
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieCard from '../MovieCard/MovieCard';
+import './MoviesContainer.scss'
+
+const MoviesContainer = () => {
+    return (
+        <section>
+            <MovieCard />
+        </section>
+    )
+}
+
+export default MoviesContainer;
