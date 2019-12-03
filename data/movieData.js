8a9e494404a54e32e5d9e419ced2541f4d34ea99
Allison Wagner <allisonjw01@gmail.com>
2019-12-03 16:23:19 -0700

    The start of mock data

---
+++ data/movieData.js
@@ -0,0 +1,46 @@
+let movieData = [
+    {
+        title: "A New Hope", 
+        episode_id: 4,
+        release_date: "1977-05-25",
+        image: ''
+    },
+    {
+        title: "Attack of the Clones", 
+        episode_id: 2,
+        release_date: "2002-05-16",
+        image: ''
+    },
+    {
+        title: "The Phantom Menace", 
+        episode_id: 1,
+        release_date: "2005-05-19",
+        image: ''
+    },
+    {
+        title: "Revenge of the Sith", 
+        episode_id: 3,
+        release_date: '1977-05-25',
+        image: ''
+    },
+    {
+        title: "Return of the Jedi", 
+        episode_id: 6,
+        release_date: "1983-05-25",
+        image: ''
+    },
+    {
+        title: "The Empire Strikes Back", 
+        episode_id: 5,
+        release_date: "1980-05-17",
+        image: ''
+    },
+    {
+        title: "The Force Awakens", 
+        episode_id: 7,
+        release_date: "2015-12-11",
+        image: ''
+    }
+];
+
+export default movieData;
