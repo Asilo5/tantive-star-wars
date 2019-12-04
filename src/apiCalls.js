5025f6b0d06d03a0fcfcc8020a741ae3344d7e57
Allison Wagner <allisonjw01@gmail.com>
2019-12-03 21:21:03 -0700

    Adds slice to get only year and first 10 characters

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,16 @@
 export const getMovies = () => {
-    return fetch('https://swapi.co/api/films/')
-      .then(res => res.json())
-      .then(films => {
-        return films.results.map((film) => {
-          const newDate = film.release_date.split('-')[0]
-          const tenCharacters = film.characters.slice(0, 10)
-          return {
-            title: film.title,
-            episode_id: film.episode_id,
-            release_date: newDate,
-            characters: tenCharacters,
-            opening_crawl: film.opening_crawl
-          }
-        })
-      })
-  }
+  return fetch('https://swapi.co/api/films/')
+    .then(res => res.json())
+    .then(movies => {
+      return movies.results.map((movie) => {
+        const firstTenCharacters = movie.characters.slice(0, 10)
+        return {
+          title: movie.title,
+          episode_id: movie.episode_id,
+          release_date: movie.release_date.slice(0, 4),
+          characters: firstTenCharacters,
+          opening_crawl: movie.opening_crawl
+        }
+      });
+    });
+  };
