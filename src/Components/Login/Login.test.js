2fe0023842d3c7e676eb359331844bc86c7fe858
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-03 19:29:16 -0700

    Add written out tests to know what to test for

--- src/Components/Login/Login.test.js
+++ src/Components/Login/Login.test.js
@@ -14,4 +14,24 @@ describe('Login', () => {
     it('should match snapshot with all information passing in correctly', () => {
         expect(wrapper).toMatchSnapshot();
     })
+
+    it('should call handleChange when input changes', () => {
+      
+    })
+
+    it('should update state when handleChange is called', () => {
+
+    })
+
+    it('should invoke handleChange on click', () => {
+        
+    })
+
+    it('should call userInfo when sendUpState is invoked', () => {
+
+    })
+
+    it('should invoke sendUpdate on click', () => {
+
+    })
 })
