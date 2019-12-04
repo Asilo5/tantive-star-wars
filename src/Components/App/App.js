7471ffbd71d344a4c7b5718e53b606bdfdb2d027
Allison Wagner <allisonjw01@gmail.com>
2019-12-03 20:12:10 -0700

    Fixed error in route path

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../Login/Login';
 import './App.scss';
-import mockData from '../../data/movieData.js'
+import mockData from '../../data/movieData.js';
+// import MoviesContainer from '../MoviesContainer/MoviesContainer'
 
 class App extends Component {
   constructor() {
@@ -27,7 +28,7 @@ class App extends Component {
       <main className="App">
         <Switch>
           <Route exact path='/' render={() => <Login userInfo={this.userInfo}/> } />
-          <Route path='/movies' render={() => <Login userInfo={this.userInfo}/> } />
+          {/* <Route path='/movies' render={() => <MoviesContainer userInfo={this.userInfo}/> } /> */}
         </Switch>
       </main>
     );
