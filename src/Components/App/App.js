0897cb2b509fb721e2d7beb3c83ba6b6472aa371
Allison Wagner <allisonjw01@gmail.com>
2019-12-03 19:59:54 -0700

    Adding api calls to app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../Login/Login';
 import './App.scss';
-import mockData from '../../data/movieData.js'
+// import mockData from '../../data/movieData.js';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import { getMovies } from '../../apiCalls';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      movies: [],
       userName: '',
       userQuote: '',
       userRank: ''
@@ -22,12 +25,19 @@ class App extends Component {
      })
   }
 
+  componentDidMount = () => {
+    getMovies()
+      .then(films => films.sort((a, b) => parseInt(a.episode_id) - parseInt(b.episode_id)))
+      .then(films => console.log(films))
+      .catch(error => console.error('error'))
+   }
+
   render() {
     return (
       <main className="App">
         <Switch>
           <Route exact path='/' render={() => <Login userInfo={this.userInfo}/> } />
-          <Route path='/movies' render={() => <Login userInfo={this.userInfo}/> } />
+          <Route path='/movies' render={() => <MoviesContainer movies={this.state.movies} userInfo={this.userInfo}/> } />
         </Switch>
       </main>
     );
