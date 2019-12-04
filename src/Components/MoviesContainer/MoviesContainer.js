6116c0963a20915fca94e6717fcffc940325dd98
Allison Wagner <allisonjw01@gmail.com>
2019-12-03 20:00:33 -0700

    Trying to get fetched film data to add

--- src/Components/MoviesContainer/MoviesContainer.js
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -2,18 +2,19 @@ import React from 'react';
 import MovieCard from '../MovieCard/MovieCard';
 import './MoviesContainer.scss'
 
-const MoviesContainer = () => {
+const MoviesContainer = ({movies}) => {
+    let allMovies = movies.map(movie => {
+    return <MovieCard 
+            title={movie.title}
+            episode_id={movie.episode_id}
+            release_date={movie.release_date}
+            id={movie.episode_id}
+            key={movie.episode_id}
+            />
+        }); 
     return (
         <section className="movieContainer">
-            <MovieCard 
-            key={}
-            id={}
-            name={}
-            homeworld={}
-            population={}
-            species={}
-            films={}
-            />
+            {allMovies}
         </section>
     )
 }
