5678d72abfa16aa5d470a5f071a6589b2febce52
Allison Wagner <allisonjw01@gmail.com>
2019-12-04 20:31:18 -0700

    Adds mock data and before each for testing

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import apiCalls  from './apiCalls';
+import { getMovies} from './apiCalls'
 
 describe('apiCalls', () => {
 
-});
+  const mockData = [{episode_id: 1},{episode_id: 2},{episode_id: 3}]
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockData)
+      })
+    });
+  })
+
+  
+})
