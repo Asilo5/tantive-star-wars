0efe1068edb6d9b2a0d9486df04321f075824bd0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-04 20:12:45 -0700

    Add basic stylin to NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,11 +5,13 @@
  const NavBar = () => {
      return (
          <nav>
-             <img src='' alt='' />
-            <h2>User Name</h2>
-            <h3>Quote</h3>
-            <h3>Rank</h3>
             <div>
+                <img src='' alt='' />
+                <h2>User Name</h2>
+                <h3>Quote</h3>
+                <h3>Rank</h3>
+            </div>
+            <div className='nav-links'>
                 <NavLink to='/movies' className='nav' activeClassName='active-nav' >Movies</NavLink>
                 <NavLink to='/favourites' className='nav' activeClassName='active-nav' >Favourites</NavLink>
                 <NavLink to='/' className='nav' activeClassName='active-nav' >Log Out</NavLink>
