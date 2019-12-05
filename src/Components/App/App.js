bbc9354e8fe2e92d0c7298f4c82c1051bac12330
Allison Wagner <allisonjw01@gmail.com>
2019-12-05 16:19:09 -0700

    Adds route component for character container to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../Login/Login';
 import './App.scss';
-import { getMovies, getCharacters } from '../../apiCalls'
-import MoviesContainer from '../MoviesContainer/MoviesContainer'
+import { getMovies, getCharacters } from '../../apiCalls';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import CharactersContainer from '../CharactersContainer/CharactersContainer';
 
 class App extends Component {
   constructor() {
@@ -47,6 +48,7 @@ class App extends Component {
           <Route exact path='/' render={() => <Login userInfo={this.userInfo}/> } />
           {isLoading ? <img className='bb8-loading' src='https://media.giphy.com/media/eEbiAqk9YUT5e/giphy.gif' alt='BB8 giff' /> : 
           (<Route exact path='/movies' render={() => <MoviesContainer movies={this.state.movies} userName={userName} userQuote={userQuote} userRank={userRank} setCharacters={this.setCharacters}/> } />)}
+          <Route path='/characters' render={() => <CharactersContainer characters={this.state.characters} /> } />
         </Switch>
       </main>
     );
