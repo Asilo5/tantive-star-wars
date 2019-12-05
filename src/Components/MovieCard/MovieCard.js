219d42bab8d6da4ae6510a32917b329209415aee
Allison Wagner <allisonjw01@gmail.com>
2019-12-05 15:27:54 -0700

    Adds an anon function to on click

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './MovieCard.scss';
 
 const MovieCard = ({title, episode_id, release_date, id, characters, openingCrawl, setCharacters }) => {
-    console.log(characters);
     return (
     <article className="movieCard">
       <ul>    
@@ -12,7 +11,7 @@ const MovieCard = ({title, episode_id, release_date, id, characters, openingCraw
         <button 
           className="movie_card_btn" 
           type="submit" 
-          onClick={setCharacters(characters)}>View Characters</button>
+          onClick={() => setCharacters(characters)}>View Characters</button>
       </ul>  
     </article>
     )
