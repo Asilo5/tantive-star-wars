9cb82ac9c50249f8cd31fe8608d9c1ef1e61a5c4
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-04 21:19:59 -0700

    Add default image of user

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,7 +6,7 @@
      return (
          <nav>
             <div>
-                <img src='' alt='' />
+                <img src='https://am22.akamaized.net/tms/cnt/uploads/2017/08/leiatop1-650x574.jpg' alt='' />
                 <h2>User Name</h2>
                 <h3>Quote</h3>
                 <h3>Rank</h3>
