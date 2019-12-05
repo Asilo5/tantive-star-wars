b9f7933f4a4042269ce715fbdcacc8fa5a35c703
Allison Wagner <allisonjw01@gmail.com>
2019-12-05 12:58:36 -0700

    Adds yoda's face to login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -26,6 +26,7 @@ class Login extends Component {
         return (
           <div>
             <h1>Star Wars Trivia</h1>
+            <img className="yoda_head" src="http://pluspng.com/img-png/yoda-head-png-download-800.gif" alt="yoda's face"></img>
             <h2 className="tantive_heading">Tantive Edition</h2>
             <form>
                 <input className='user-name'
@@ -42,9 +43,29 @@ class Login extends Component {
                        onChange={(e) => this.handleChange(e)}/>
                 <p>What Knowledge have you from Star Wars?</p>
                 <section>
-                    <button className="knowledge_button" type='button' name='rank' value='Novice' onClick={(e) => this.handleChange(e)} >Novice</button>
-                    <button className="knowledge_button" type='button' name='rank' value='Intermediate' onClick={(e) => this.handleChange(e)}>Intermediate</button>
-                    <button className="knowledge_button" type='button' name='rank' value='Expert' onClick={(e) => this.handleChange(e)}>Expert</button>
+                    <button 
+                      className="knowledge_button" 
+                      type='button' 
+                      name='rank' 
+                      value='Novice' 
+                      onClick={(e) => this.handleChange(e)} 
+                      >Novice
+                    </button>
+                    <button 
+                      className="knowledge_button" 
+                      type='button' 
+                      name='rank' 
+                      value='Intermediate' 
+                      onClick={(e) => this.handleChange(e)}>Intermediate
+                    </button>
+                    <button 
+                      className="knowledge_button"  
+                      type='button' 
+                      name='rank' 
+                      value='Expert' 
+                      onClick={(e) => this.handleChange(e)}
+                      >Expert
+                    </button>
                 </section>
                 { name === '' || quote === '' || rank === '' ? 
                 ( <button className='submit-btn-error' disabled={true}> Inputs Must Fill </button>)
