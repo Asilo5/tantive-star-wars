6c7ae500a86d0b83155faa63e06b500fa53f1ccb
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-04 19:56:15 -0700

    Add stylinf elements to NavLinks

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,19 @@
  import React from 'react';
  import './NavBar.scss';
+ import { NavLink } from 'react-router-dom';
 
  const NavBar = () => {
      return (
          <nav>
-
+             <img src='' alt='' />
+            <h2>User Name</h2>
+            <h3>Quote</h3>
+            <h3>Rank</h3>
+            <div>
+                <NavLink to='/movies' className='nav' activeClassName='active-nav' >Movies</NavLink>
+                <NavLink to='/favourites' className='nav' activeClassName='active-nav' >Favourites</NavLink>
+                <NavLink to='/' className='nav' activeClassName='active-nav' >Log Out</NavLink>
+            </div>
          </nav>
      )
  }
