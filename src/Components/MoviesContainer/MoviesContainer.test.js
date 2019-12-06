a7e67cbc0e6a3eee7594831df7c7d5533008c42f
Allison Wagner <allisonjw01@gmail.com>
2019-12-05 19:53:18 -0700

    Sets up data for snapshot

--- src/Components/MoviesContainer/MoviesContainer.test.js
+++ src/Components/MoviesContainer/MoviesContainer.test.js
@@ -3,9 +3,25 @@ import { shallow } from 'enzyme';
 import MoviesContainer from './MoviesContainer';
 
 describe('MoviesContainer', () => {
-    let wrapper = shallow(<MoviesContainer />)
 
     it('should match snapshot with all information passing in correctly', () => {
+        const wrapper = shallow(<MoviesContainer movies={[
+            {
+              title: 'The Phantom Menace',
+              episode_id: 1,
+              release_date: 2005,
+              id: 1,
+            },
+            {
+              title: 'Revenge of the Sith',
+              episode_id: 3,
+              release_date: 1977,
+              id: 3,
+            }
+          ]} 
+            getCharacters={jest.fn()} />
+    
+        )
         expect(wrapper).toMatchSnapshot();
     });
 
