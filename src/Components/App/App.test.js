ff6fe8c49afdb79cb11e2a3f7236d821b366149b
Allison Wagner <allisonjw01@gmail.com>
2019-12-06 12:51:38 -0700

    Basic layout of testing

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+      wrapper = shallow(<App />)
+  });
+
+  
+
+});
