ae9625586a46d174eeea984a8f6132baf9516368
Allison Wagner <allisonjw01@gmail.com>
2019-12-06 13:42:00 -0700

    Starting test for userInfo method

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -13,4 +13,49 @@ describe('App', () => {
       expect(wrapper).toMatchSnapshot();
   });
 
+  it('should have an empty state to before login', () => {
+    expect(wrapper.state()).toEqual({
+        movies: [],
+        characters: [],
+        userName: '',
+        userQuote: '',
+        userRank: '',
+        isLoading: true
+    });
+  });
+
+  it('should update state when user submits form on login', () => {
+      const mockUserInfo = {
+        userName: 'Consuelo',
+        userQuote: 'Go Forth I Will',
+        userRank: 'Expert'
+      }
+
+      wrapper.instance().userInfo(mockUserInfo);
+      expect(wrapper.state('userName')).toEqual('Consuelo');
+      expect(wrapper.state('userQuote')).toEqual('Go Forth I Will');
+      expect(wrapper.state('userRank')).toEqual('Expert');
+  });
+
+  it('should retrieve Movies after mounting', () => {
+      const movieData = [
+        {
+          title: 'A New Hope',
+          episode: 4
+          releaseYear: '1974',
+          characters: 
+          scrollText: 'blah blah',
+        }, {
+          title: 'Whatever',
+          episode: 7
+          releaseYear: '1985',
+          characters:
+          scrollText: 'blah blah',
+        }
+      ];
+
+      wrapper.instance().setMovie(['the land before time', 'the land before time ii'])
+      expect(wrapper.state('movies')).toEqual(movieData)
+  });
+
 });
