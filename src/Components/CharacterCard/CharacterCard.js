ee5433457bef074a71d3b34f6610815cced3b9c7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-05 19:16:37 -0700

    Add star wars icon to card

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './CharacterCard.scss';
+import { FaJedi } from "react-icons/fa";
 
 const CharacterCard = ({ name, homeworld, population, species, films }) => {
   return (
       <section className='card'>
+        <FaJedi />
         <h2>{name}</h2>
         <h3>Homeworld:</h3>
         <p>{homeworld}</p>
