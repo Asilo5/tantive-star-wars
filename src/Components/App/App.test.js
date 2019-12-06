aa1fd032f944997eb22397f6e8c54584bfeacac0
Allison Wagner <allisonjw01@gmail.com>
2019-12-06 14:54:20 -0700

    Reworking before each with mock movie data

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,47 +1,87 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { getMovies } from '../../apiCalls'
+
+jest.mock('../../apiCalls.js')
 
 describe('App', () => {
-  let wrapper;
+  let wrapper,
+      mockMovies;
 
   beforeEach(() => {
-      wrapper = shallow(<App />)
+    mockMovies = [
+        {
+          title: 'The Phantom Menace', 
+          episode_id: 1, 
+          release_date: '1999',
+          characters: ['person'], 
+          opening_crawl: 'Turmoil has engulfed...'
+        }, 
+        {
+          title: 'Attack of the Clones', 
+          episode_id: 2, 
+          release_date: '2002',
+          characters: ['person'], 
+          opening_crawl: 'Turmoil has engulfed...'
+        }
+    ]
+    
+    getMovies.mockImplementation(() => {
+      return Promise.resolve(mockMovies);
+    });
+    wrapper = shallow(<App />)
   });
 
   it('should match a snapshot of all the information being passed in correctly', () => {
       expect(wrapper).toMatchSnapshot();
   });
 
-  it('should have an empty state to before login', () => {
-    expect(wrapper.state()).toEqual({
-        movies: [],
-        characters: [],
-        userName: '',
-        userQuote: '',
-        userRank: '',
-        isLoading: true
-    });
+  it('should retrieve movies after mounting', () => {
+    shallow(<App />)
+    expect(getMovies).toHaveBeenCalled();
   });
 
-  it('should update state when user submits form on login', () => {
+//   it.skip('should have an empty state to before login', () => {
+//     expect(wrapper.state()).toEqual({
+//         movies: [],
+//         characters: [],
+//         userName: '',
+//         userQuote: '',
+//         userRank: '',
+//         isLoading: true
+//     });
+//   });
+
+  it.skip('should update state when user submits form on login', () => {
       const mockUserInfo = {
         userName: 'Consuelo',
         userQuote: 'Go Forth I Will',
         userRank: 'Expert'
       }
 
+      expect(wrapper.state('userName')).toEqual('');
+      expect(wrapper.state('userQuote')).toEqual('');
+      expect(wrapper.state('userRank')).toEqual('');
       wrapper.instance().userInfo(mockUserInfo);
       expect(wrapper.state('userName')).toEqual('Consuelo');
       expect(wrapper.state('userQuote')).toEqual('Go Forth I Will');
       expect(wrapper.state('userRank')).toEqual('Expert');
   });
 
-  it('should retrieve movies after mounting', () => {
-      const movieData = [{episode_id: 1},{episode_id: 2},{episode_id: 3}];
+  it('should update state with an movies when getMovies is called', async () => {
+     getMovies.mockImplementation(() => {
+         return Promise.resolve(
+         { title: 'The Phantom Menace', episode_id: 1, release_date: '1999',characters: ['person'], opening_crawl: 'Turmoil has engulfed...'}
+         );
+     })
+     const mockIdea = { title: 'The Phantom Menace', episode_id: 1, release_date: '1999',characters: ['person'], opening_crawl: 'Turmoil has engulfed...'};
+     const expected = [{ title: 'The Phantom Menace', episode_id: 1, release_date: '1999',characters: ['person'], opening_crawl: 'Turmoil has engulfed...'}, mockIdea];
 
-      wrapper.instance().componentDidMount(['The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith'])
-      expect(wrapper.state('movies')).toEqual(movieData)
-  });
+     await wrapper.instance().getMovies(mockIdea);
+
+     expect(getMovies).toHaveBeenCalledWith(mockIdea);
+     expect(wrapper.state('movies')).toEqual(expected);
+    });
 
 });
