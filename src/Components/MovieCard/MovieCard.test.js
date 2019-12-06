5ae5c861db88a8d2fa20ffcbfddf40c892ddc9cc
Allison Wagner <allisonjw01@gmail.com>
2019-12-05 19:15:57 -0700

    Add additonal information for snapshot test

--- src/Components/MovieCard/MovieCard.test.js
+++ src/Components/MovieCard/MovieCard.test.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MovieCard from './MovieCard';
+
+describe('MovieCard', () => {
+
+    let wrapper = shallow(<MovieCard />)
+
+    it('should match snapshot with all information passing in correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+});
