019ae05e1dc4fad67734e02fdcbbaa7042a847d6
Allison Wagner <allisonjw01@gmail.com>
2019-12-07 16:58:06 -0700

    Adds card to return

--- src/Components/FavouritesContainer/FavouritesContainer.js
+++ src/Components/FavouritesContainer/FavouritesContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './FavouritesContainer.scss';
-import CharacterCard from '../CharacterCard/CharacterCard';
+// import CharacterCard from '../CharacterCard/CharacterCard';
+import { FaJedi } from "react-icons/fa";
 
 
-const FavouritesContainer = ({ characters, toggleFavourites, favourites }) => {
-    if(favourites.length===0) {
+const FavouritesContainer = ({ id, name, homeworld, population, species, films,toggleFavourites, favourites }) => {
+    if(favourites.length === 0) {
       return (
         <>
         <h2 className='fav_h2'>Want to see your favorite character!</h2>
@@ -16,15 +17,22 @@ const FavouritesContainer = ({ characters, toggleFavourites, favourites }) => {
         </>
      ) 
     } 
-    const allCharacters = characters.map((character) => {
-      return <CharacterCard key={character.name} {...character} toggleFavourites={toggleFavourites} favourites={favourites} />
-    });
-
-    return (
-     <section className='characters'>
-       {allCharacters} 
-     </section>
-    )
+    const theFilm = films.map((film, i) => <p key={i}>{film}</p>);
+  const toggleIcon = favourites.includes(id) ? 'favourite-icon' : 'jedi-icon';
+  return (
+      <section className='card'>
+        <p className={toggleIcon} onClick={() => toggleFavourites(id)}><FaJedi/></p>
+        <h2>{name}</h2>
+        <h3>Homeworld:</h3>
+        <p>{homeworld}</p>
+        <h3>Population:</h3>
+        <p>{population}</p>
+        <h3>Species: </h3>
+        <p>{species}</p>
+        <h3>Seen in:</h3>
+        {theFilm}
+      </section>
+  ) 
 }
 
 
