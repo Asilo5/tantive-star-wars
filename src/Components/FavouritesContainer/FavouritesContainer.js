6abb0db84a4b79b744c0ef4bfe350ba69483f701
Allison Wagner <allisonjw01@gmail.com>
2019-12-07 15:46:27 -0700

    Start favourite component

---
+++ src/Components/FavouritesContainer/FavouritesContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './FavouritesContainer.scss';
+
+const FavouritesContainer = () => {
+    return (
+        <>
+        <h2>Your Favourites Characters</h2>
+        </>
+    )
+}
+
+
+export default FavouritesContainer;
