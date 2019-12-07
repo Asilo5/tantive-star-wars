ee913acb53a71f4a520552548e0db08ef51c7e19
Allison Wagner <allisonjw01@gmail.com>
2019-12-07 16:18:47 -0700

    Adding props to favourite component

--- src/Components/FavouritesContainer/FavouritesContainer.js
+++ src/Components/FavouritesContainer/FavouritesContainer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './FavouritesContainer.scss';
+import CharacterCard from '../CharacterCard/CharacterCard';
 
-const FavouritesContainer = () => {
-    return (
-        <>
-        <h2>Your Favourites Characters</h2>
-        </>
-    )
+
+const FavouritesContainer = ({ characters, toggleFavourites, favourites }) => {
+    const allCharacters = characters.map((character) => {
+        return <CharacterCard key={character.name} {...character} toggleFavourites={toggleFavourites} favourites={favourites} />
+    })
+   return (
+           <section className='characters'>
+             {allCharacters} 
+           </section>
+   )
 }
+
 
 
 export default FavouritesContainer;
