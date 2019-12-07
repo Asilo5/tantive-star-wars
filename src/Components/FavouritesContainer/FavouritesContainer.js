d0f9f4cfa90097bb9cc68086dd5e63c5c4876623
Allison Wagner <allisonjw01@gmail.com>
2019-12-07 16:45:27 -0700

    Adds message if no favs are selected

--- src/Components/FavouritesContainer/FavouritesContainer.js
+++ src/Components/FavouritesContainer/FavouritesContainer.js
@@ -5,14 +5,26 @@ import CharacterCard from '../CharacterCard/CharacterCard';
 
 
 const FavouritesContainer = ({ characters, toggleFavourites, favourites }) => {
+    if(favourites.length===0) {
+      return (
+        <>
+        <h2 className='fav_h2'>Want to see your favorite character!</h2>
+          <h3 className='fav_h3'>Go back to movies and select a few first.</h3>
+          <Link to='/movies'>
+            <button className='back_to_movies__btn'>Back to Movies</button>
+          </Link>
+        </>
+     ) 
+    } 
     const allCharacters = characters.map((character) => {
-        return <CharacterCard key={character.name} {...character} toggleFavourites={toggleFavourites} favourites={favourites} />
-    })
-   return (
-           <section className='characters'>
-             {allCharacters} 
-           </section>
-   )
+      return <CharacterCard key={character.name} {...character} toggleFavourites={toggleFavourites} favourites={favourites} />
+    });
+
+    return (
+     <section className='characters'>
+       {allCharacters} 
+     </section>
+    )
 }
 
 
