83dcac90045d956bec9568028eabc919d7c04581
Allison Wagner <allisonjw01@gmail.com>
2019-12-08 14:10:14 -0700

    Removes extra lines

--- src/Components/FavouritesContainer/FavouritesContainer.js
+++ src/Components/FavouritesContainer/FavouritesContainer.js
@@ -5,16 +5,16 @@ import CharacterCard from '../CharacterCard/CharacterCard';
 
 
 const FavouritesContainer = ({ toggleFavourites, favourites, characters }) => {
-    if(favourites === 0) {
+    if(favourites.length === 0) {
       return (
-        <section className='favourite'>
+        <section className='blank_error'>
         <h2 className='fav_h2'>Want to see your favorite character!</h2>
           <h3 className='fav_h3'>Go back to movies and select a few first.</h3>
           <Link to='/movies'>
             <button className='back_to_movies__btn'>Back to Movies</button>
           </Link>
         </section>
-     ) 
+      ) 
     } 
 
 const characterList = characters.map((character) => {
