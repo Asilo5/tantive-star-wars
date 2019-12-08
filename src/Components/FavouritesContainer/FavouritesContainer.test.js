7b23082f438ed517aea12ec1042c46117d5c1be2
Allison Wagner <allisonjw01@gmail.com>
2019-12-08 13:47:00 -0700

    Mocks data being passed

--- src/Components/FavouritesContainer/FavouritesContainer.test.js
+++ src/Components/FavouritesContainer/FavouritesContainer.test.js
@@ -3,11 +3,18 @@ import { shallow } from 'enzyme';
 import FavouritesContainer from './FavouritesContainer';
 
 describe('FavouritesContainer', () => {
-    let wrapper;
+  let wrapper
+  const mockChangeSelectedMovie = jest.fn();
+  const mockSetFavorite = jest.fn();
 
-    wrapper = shallow(<FavouritesContainer />);
+  it('should match the snapshot with all the data passed in correctly', () => {
+      wrapper = shallow(<FavouritesContainer
+          characters={[]}
+          changeSelectedMovie={mockChangeSelectedMovie}
+          isReady={false}
+          setFavorite={mockSetFavorite}
+          favoriteList={[{}, {}]} />)
+      expect(wrapper).toMatchSnapshot();
+  });
 
-    it.skip('should match snapshot with all information passing in correctly', () => {
-        expect(wrapper).toMatchSnapshot()
-      })
 });
