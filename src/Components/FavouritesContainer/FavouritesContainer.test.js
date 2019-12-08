c5c796bf6e7bd04ab2787e73e52cdc7c14f67965
Allison Wagner <allisonjw01@gmail.com>
2019-12-08 14:02:01 -0700

    Adds test for snapshot

--- src/Components/FavouritesContainer/FavouritesContainer.test.js
+++ src/Components/FavouritesContainer/FavouritesContainer.test.js
@@ -4,16 +4,22 @@ import FavouritesContainer from './FavouritesContainer';
 
 describe('FavouritesContainer', () => {
   let wrapper
-  const mockChangeSelectedMovie = jest.fn();
-  const mockSetFavorite = jest.fn();
+  const mocktoggleFavourites = jest.fn();
 
   it('should match the snapshot with all the data passed in correctly', () => {
       wrapper = shallow(<FavouritesContainer
-          characters={[]}
-          changeSelectedMovie={mockChangeSelectedMovie}
-          isReady={false}
-          setFavorite={mockSetFavorite}
-          favoriteList={[{}, {}]} />)
+        name = 'Leia'
+        homeWorld = 'Alderan'
+        population = '200000000000'
+        species = 'Human'
+        films = {[
+            "The Empire Strikes Back",
+            "Revenge of the Sith",
+            "Return of the Jedi"
+        ]}
+        characters={[]}
+        toggleFavourites={mocktoggleFavourites}
+        favorites={[]} />)
       expect(wrapper).toMatchSnapshot();
   });
 
