16053c73132d9c7157cfd1dda9a753c129a2cbcd
Allison Wagner <allisonjw01@gmail.com>
2019-12-08 17:28:34 -0700

    Add roman numerals for the episodes

--- src/Components/ScrollText/ScrollText.js
+++ src/Components/ScrollText/ScrollText.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './ScrollText.scss';
 
 const ScrollText = ({ movies }) => {
+  let romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+  let romanEpisodeID = romanNumerals[parseInt(movies.episode_id) - 1]
     return (
-        <section className="scroll">
-            <div>
-                {movies.opening_crawl}
-            </div>
-        </section>
+        <article className="scroll">
+            <section className="text">
+                <p>Episode {romanEpisodeID}</p>
+              <h3 className="episode_title">{movies.title}</h3>
+              <p>{movies.opening_crawl}</p>
+            </section>
+        </article>
     )
 }
 
