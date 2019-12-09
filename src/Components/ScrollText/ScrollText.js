b52770383e738c26cc684f720bf2b83e2ea3937e
Allison Wagner <allisonjw01@gmail.com>
2019-12-08 18:48:19 -0700

    Adds mapping of movies

--- src/Components/ScrollText/ScrollText.js
+++ src/Components/ScrollText/ScrollText.js
@@ -4,14 +4,23 @@ import './ScrollText.scss';
 const ScrollText = ({ movies }) => {
   let romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
   let romanEpisodeID = romanNumerals[parseInt(movies.episode_id) - 1]
+  let movieSet = movies.map(movie => {
     return (
         <article className="scroll">
+          <div className="scrollText"> 
             <section className="text">
-                <p>Episode {romanEpisodeID}</p>
-              <h3 className="episode_title">{movies.title}</h3>
-              <p>{movies.opening_crawl}</p>
+              <p>Episode {romanEpisodeID}</p>
+              <h3 className="episode_title">{movie.title}</h3>
+              <p>{movie.opening_crawl}</p>
             </section>
+          </div>  
         </article>
+    )
+  });
+    return (
+        <>
+        {movieSet}
+        </>
     )
 }
 
