fa587cef05ac8dac70ce6c928b28b53f306fcce9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-09 19:13:18 -0700

    Remove unwanted commented out code

--- src/Components/ScrollText/ScrollText.js
+++ src/Components/ScrollText/ScrollText.js
@@ -6,17 +6,6 @@ const ScrollText = ({ movieScroll }) => {
   //       let romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
   //       let romanEpisodeID = romanNumerals[parseInt(movie.episode_id) - 1]
      
-  //   return (
-  //       <article className="scroll">
-  //         <div className="scrollText"> 
-  //           <section className="text">
-  //             <p>Episode {romanEpisodeID}</p>
-  //             <h3 className="episode_title">{movie.title}</h3>
-  //             <p>{movie.opening_crawl}</p>
-  //           </section>
-  //         </div>  
-  //       </article>
-  //   )
   // });
 
   console.log(movieScroll)
