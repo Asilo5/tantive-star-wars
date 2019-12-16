5646b120c71dc9e6f425fcf03460c8ccf31ba571
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-15 20:08:20 -0700

    Updates

---
+++ precache-manifest.82eb0eb3c65f6d0df37c5d97eedf0a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6600dfdf52d5f199dbf423669f8e192d",
+    "url": "/tantive-star-wars/index.html"
+  },
+  {
+    "revision": "6faa53fd2a53eaa17f9c",
+    "url": "/tantive-star-wars/static/css/main.2ad6c1e1.chunk.css"
+  },
+  {
+    "revision": "b87bc8d5314eac0c39b1",
+    "url": "/tantive-star-wars/static/js/2.e5728cca.chunk.js"
+  },
+  {
+    "revision": "6faa53fd2a53eaa17f9c",
+    "url": "/tantive-star-wars/static/js/main.8b062e9a.chunk.js"
+  },
+  {
+    "revision": "9c09a6e92d0aa8ffbd59",
+    "url": "/tantive-star-wars/static/js/runtime-main.99400357.js"
+  }
+]);
